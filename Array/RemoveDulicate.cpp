32ca5bf2ddb64e989174b52c451613d7e126ae03
amritanshu11 <amritanshu110901@gmail.com>
2022-06-01 13:45:28 +0530

    commit

--- Array/RemoveDulicate.cpp
+++ Array/RemoveDulicate.cpp
@@ -1,31 +1,28 @@
 // Remove Duplicates from Sorted Array
 #include <bits/stdc++.h>
 using namespace std;
- 
-int main(){
-int n;
-cin>>n;
-int arr[n];
-for (int i = 0; i < n; i++)
+
+int main()
 {
-  cin>>arr[i];
-}
-map<int,int>mp;
-for (int i = 0; i < n; i++)
-{
-    if (mp.find(arr[i]) == mp.end())
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
     {
-     mp[arr[i]]++;
-    }   
-}
-for(auto i:mp){
-        cout<<i.first<<" ";
-}
+        cin >> arr[i];
+    }
+    map<int, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        if (mp.find(arr[i]) == mp.end())
+        {
+            mp[arr[i]]++;
+        }
+    }
+    for (auto i : mp)
+    {
+        cout << i.first << " ";
+    }
 
-
-
-
-
- 
-return 0;
+    return 0;
 }
